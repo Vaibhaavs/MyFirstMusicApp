3f670373361c58962582b5edae05ad0a5e8b511d
Vaibhaav <2103377D@student.tp.edu.sg>
2021-04-25 18:18:54 +0800

    Default Project

---
+++ app/src/main/java/tp/edu/myjavaapp/MyJavaApp.java
@@ -0,0 +1,49 @@
+package tp.edu.myjavaapp;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class MyJavaApp {
+    public static void main(String[] args) {
+        String name = "Mary";
+        int age = 5;
+        double height = 1.23;
+        char grade = 'A';
+        boolean child = true;
+
+
+        System.out.print("Name:" + name);
+        System.out.print(" Age:" + age);
+        System.out.println(" Height:" + height);
+
+
+        String name2 = "John";
+        double lengthOfTable = 1.6;
+
+        System.out.println("Hello World!");
+        System.out.println("My name is " + name2);
+        System.out.print(" The length of this table is " + lengthOfTable + "m");
+
+        String nameofflower = " Angelica ";
+
+        System.out.print(" The name of this flower is " + nameofflower );
+
+
+        double priceofIphone=1199.9;
+
+        System.out.print( "The price of the iphone is  " +"$"+priceofIphone);
+
+        int age1 = 17;
+
+        System.out.print(" The age of a student "+ age1);
+
+
+        char grade1 = 'B';
+        System.out.print("  The Chemistry Grade is " + grade1);
+
+
+        boolean rain = true;
+        System.out.print("It is" + rain +"  that the rain has stopped  ");
+
+
+    }
+}
